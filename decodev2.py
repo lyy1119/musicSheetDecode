41887cba994824b9531d82a530c11630aa9f6788
ADreamToday <adreamtoday@163.com>
2023-03-22 21:23:52 +0800

    debug并增加新功能中

--- decodev2.py
+++ decodev2.py
@@ -284,6 +284,75 @@ def SaveSoundTrack(lists , enviro):
             address = address + 1
 
 
+def transSTCcode(alist):
+    stcSheet = [[65,    73,   82,   87,   98,  110,  123],
+                [69,    78,  0  ,   92,  104,  116,    0],
+                [131,   147,  165,  175,  196,  220,  247],
+                [139,   155,  0  ,  185,  208,  233,    0],
+                [262,   294,  329,  349,  392,  440,  494],
+                [277,   311,  0  ,  370,  415,  466,    0],
+                [523 ,  587 , 659 , 698 , 784 , 880 , 988],
+                [554 ,  622 , 0   , 740 , 831 , 932 ,   0],
+                [1046, 1174, 1318, 1396, 1567, 1759, 1975],
+                [1108, 1244,  0  , 1479, 1660, 1864,    0],
+                [2092, 2348, 2636, 2792, 3134, 3518, 3949],
+                [2216, 2488,  0  , 2959, 3321, 3728,    0],
+                [4184, 4696, 5272, 5585, 6269, 7037, 7898],
+                [4433, 4976,  0  , 5917, 6642, 7455,    0]]
+    
+    localCodeSheet = [[-20 , -19 , -18 , -17 , -16 , -15 , -14],
+                             [-19.5,-18.5,-17.5,-16.5,-15.5,-14.5,-13.5],
+                             [-13,-12,-11,-10,-9,-8,-7],
+                             [-12.5,-11.5,-10.5,-9.5,-8.5,-7.5,-6.5],
+                             [-6,-5,-4,-3,-2,-1,0],
+                             [-5.5,-4.5,-3.5,-2.5,-1.5,-0.5,0.5],
+                             [1,2,3,4,5,6,7],
+                             [1.5,2.5,3.5,4.5,5.5,6.5,7.5],
+                             [8,9,10,11,12,13,14],
+                             [8.5,9.5,10.5,11.5,12.5,13.5,14.5],
+                             [15,16,17,18,19,20,21],
+                             [15.5,16.5,17.5,18.5,19.5,20.5,21.5],
+                             [22,23,24,25,26,27,28],
+                             [22.5,23.5,24.5,25.5,26.5,27.5,28.5]]
+    res = []
+    stopsign = 0
+    for i in alist:
+        for j in range(14): # 行坐标
+            if stopsign == 1:
+                stopsign = 0
+                break
+            for k in range(7): # 列坐标
+                if abs(i - localCodeSheet[j][k]) < 0.01:
+                    res.append(stcSheet[j][k])
+                    print(str(i) + "[%d][%d]" % (j , k), end=" -> ")
+                    print(stcSheet[j][k])
+                    stopsign = 1
+                    break
+                if abs(i - 29) < 0.01:
+                    res.append(0)
+                    print(str(i) + "休止符", end=" -> ")
+                    print(0)
+                    stopsign = 1
+                    break
+                
+                
+                
+    return res
+    
+def STCcodeSave(lists):
+    with open("output" , "a") as f:
+        f.write("\nSTC播放码:\n")
+        num = 1
+        for i in lists:
+            f.write("soundTrack%d = {" % num)
+            for j in i:
+                f.write("%d ," % j)
+            f.write('}\n')
+            num = num + 1
+
+
+
+
 
 if __name__ == "__main__":
 
@@ -339,9 +408,17 @@ if __name__ == "__main__":
     if "print_on_cmd" in envir.keys():
         if int(envir["print_on_cmd"]) != 0:
             PrintSoundTrack(soundtracks)
+
     if "output_off" in envir.keys():
         if int(envir["output_off"]) != 0:
             pass
     else:
         SaveSoundTrack(soundtracks , envir)
+
+    stcCodes = []
+    if "stc" in envir.keys():
+        if int(envir["stc"]) != 0:
+            for i in soundtracks:
+                stcCodes.append(transSTCcode(i))
+            STCcodeSave(stcCodes)
     # ==================================================================
