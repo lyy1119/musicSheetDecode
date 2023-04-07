7ed8ad51918c41810ee39c3ea3d90613c2be4c1d
ADreamToday <107332201+ADreamToday@users.noreply.github.com>
2023-04-07 23:29:26 +0800

    基本结构大改完成

--- decode4.py
+++ decode4.py
@@ -39,6 +39,7 @@ def decodeEnviro(code):
     print("===Environment Decode Finish===\n")
     return res
 
+
 def decodeSections(code):
     print("===Decode Section===")
 	
@@ -97,11 +98,127 @@ def basicDecode():
 	
     print("=====Basci Decode Finish=====\n")
 
-    print(envirs)
-    print(sections)
-    print(main)
-    print(inputsWithRealCode)
+    # print(envirs)
+    # print(sections)
+    # print(main)
+    # print(inputsWithRealCode)
     return envirs , sections , main
+
+def envirsgen(envirs):
+    # generate stdfreq
+    # tunes = []
+    if "mainTune" in envirs.keys():
+        tune = envirs["mainTune"]
+        print("Set tune %s" % tune)
+        tunes = soundfreq.stdfreq(tune)
+    else:
+        tunes = soundfreq.stdfreq('C')
+    
+    # get speed
+    if "speed" in envirs.keys():
+        speed = envirs["speed"]
+    else:
+        speed = 1
+
+    return tunes , speed
+
+# 把所有段乐谱符号解析 ， 包括主乐谱
+def decodeSectionSign(sections , main):
+    # 先解析段
+    # 采用覆写section的方法
+    # sections = {'':[]}
+    new_sections = {}
+    for i in sections.keys():
+        new_sections[i] = []
+
+        for j in sections[i]:
+
+            if j.startswith('='):
+                # 将一个section塞进另一个
+                # 按照逻辑，将要塞的section必须已经载入new_sections
+                section_name = j.split('=')[1]
+                new_sections[i].extend(new_sections[section_name])
+            else:
+                # 将原有的值直接复制到新的sections
+                new_sections[i].append(j)
+
+
+    # 覆写完成 解析并覆写main
+    # main = []
+    new_main = []
+    for i in main:
+        if i.startswith('='):
+            section_name = i.split('=')
+            new_main.extend(new_sections[section_name])
+        else:
+            new_main.append(i)
+
+    return new_main
+
+# 解析一个乐谱音符
+def singleSignDecode(astr):
+    std = 35    # 12 + 12 + 12 + 1 - 1 - 1
+    # 一个 - 号 减12
+    # 一个+号 加12
+
+    res = [0 , 0.0]
+
+    # 先解析 - + 号
+    plus = astr.count('+')
+    minus = astr.count('-')
+
+    change = plus*12 + minus*(-12)
+
+    # 解析 * 号
+    if plus != 0:
+        remain = astr.split('+')[plus]
+    elif minus != 0:
+        remain = astr.split('-')[minus]
+    else:
+        remain = astr
+
+    # 处理时间
+    if '*' in remain:
+        res[1] = float(remain.split('*')[1])
+    else:
+        res[1] = float(1.0)
+
+    # 处理序号
+    nums = {'1':1,
+            '2':3,
+            '3':5,
+            '4':6,
+            '5':8,
+            '6':10,
+            '7':12}
+    num = 0
+    remain = remain.split('*')[0]
+    if 'b' in remain:
+        num = -1 + nums[remain.split('b')[0]]
+    elif '#' in remain:
+        num = 1 + nums[remain.split('#')[0]]
+    else:
+        num = nums[remain]
+
+    num = num + change + std
+    res[0] = num
+
+    # 删除了判断&号
+
+    # res = [freq(num) , time(double)]
+    return res
+
+
+# 解析底层
+def decodeSheet(main , tunes):
+    main_freq = []
+    for i in main:
+        main_freq.append([tunes[singleSignDecode(i)[0]] , singleSignDecode(i)[1]])
+    return main_freq
 
 if __name__ == "__main__":
     envirs , sections , main = basicDecode()
+    tunes , speed = envirsgen(envirs)
+    main = decodeSectionSign(sections , main)
+    main_freq = decodeSheet(main , tunes)
+    print(main_freq)
--- soundfreq.py
+++ soundfreq.py
@@ -1,21 +1,35 @@
 import decimal
 
 
-bi = decimal.Decimal(2)**decimal.Decimal(1/12)
 
-def stdfreq():
-    # 低
-    pre = decimal.Decimal(523)
+
+def stdfreq(musictune):
+    bi = decimal.Decimal(2)**decimal.Decimal(1/12)
     freq = decimal.Decimal(523)
+    stdtune = {'A': -3,
+               'A#': -2,'Bb':-2,
+               'B':-1,
+               'C':0,
+               'C#':1,'Db':1,
+               'D':2,
+               'D#':3,'Eb':3,
+               'E':4,
+               'F':5,
+               'F#':6,'Gb':6,
+               'G':7}
+    tunechange = int(stdtune[musictune])
+    if tunechange != 0:
+        freq = freq * (bi**tunechange)
+    
+    freq = freq * (bi**(-36))
 
-    pre = freq
-    sharp_freq = []
     freqs = []
-    for i in range(24):
-        print(freq)
+    for i in range(84):
         freqs.append(int(freq.quantize(decimal.Decimal('0'))))
         freq = freq*bi
+    # print(freqs[36])
+    return freqs
 
-
-    print(freqs)
+if __name__ == "__main__":
+    stdfreq('C')
 
